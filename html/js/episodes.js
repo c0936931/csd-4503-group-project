9b22e32b05d7c604f6154642d1e786468200b1e3
c0936931 <c0936931@mylambton.ca>
2025-12-04 20:43:48 -0500

    Update to allow for episode 1 to be a link

--- html/js/episodes.js
+++ html/js/episodes.js
@@ -1,40 +1,60 @@
 const data = {
-	"episodes": [{
-			"label": "Episode 1",
-			"value": "./episodes/episode1.mp4"
-		},
-		{
-			"label": "Episode 2",
-			"value": "./episodes/episode2.mp4"
-		},
-		{
-			"label": "Episode 3",
-			"value": "./episodes/episode3.mp4"
-		},
-		{
-			"label": "Episode 4",
-			"value": "./episodes/episode4.mp4"
-		}
-	]
+  "episodes": [
+    {
+      "label": "Episode 1",
+      "value": "https://mylambton.sharepoint.com/:v:/s/Section_191279-TeamScrumStormers/EU492MBEZ91Ah2KmUby3qcYBNZZ8y-ccDni8wXPMAD_4qQ?e=Junlvx",
+      "type": "link"
+    },
+    {
+      "label": "Episode 2",
+      "value": "./episodes/episode2.mp4",
+      "type": "video"
+    },
+    {
+      "label": "Episode 3",
+      "value": "./episodes/episode3.mp4",
+      "type": "video"
+    },
+    {
+      "label": "Episode 4",
+      "value": "./episodes/episode4.mp4",
+      "type": "video"
+    }
+  ]
 };
 
 const episodeSelect = document.getElementById('episodeSelect');
 const episodePlayer = document.getElementById('episodePlayer');
 const currentEpisode = document.getElementById('currentEpisode');
 
+// Populate dropdown
 data.episodes.forEach((ep, index) => {
-	const option = document.createElement('option');
-	option.value = ep.value;
-	option.textContent = ep.label;
-	if (index === 0) option.selected = true;
-	episodeSelect.appendChild(option);
+  const option = document.createElement('option');
+  option.value = ep.value;
+  option.textContent = ep.label;
+  if (index === 0) option.selected = true;
+  episodeSelect.appendChild(option);
 });
 
-episodePlayer.src = data.episodes[0].value;
-currentEpisode.textContent = data.episodes[0].label;
+// Function to display episode
+function displayEpisode(ep) {
+  if (ep.type === "video") {
+    episodePlayer.style.display = "block";
+    episodePlayer.src = ep.value;
+    currentEpisode.textContent = ep.label;
+  } else {
+    episodePlayer.style.display = "none";
+    currentEpisode.innerHTML = `${ep.label} - 
+      <a href="${ep.value}" target="_blank" rel="noopener">Watch here</a> 
+      <span style="color:gray; font-size:0.9em;">(Link provided as file was too large)</span>`;
+  }
+}
 
+// Initialize first episode
+displayEpisode(data.episodes[0]);
+
+// Handle selection change
 episodeSelect.addEventListener('change', () => {
-	const selectedOption = episodeSelect.options[episodeSelect.selectedIndex];
-	episodePlayer.src = selectedOption.value;
-	currentEpisode.textContent = selectedOption.text;
+  const selectedEpisode = data.episodes[episodeSelect.selectedIndex];
+  displayEpisode(selectedEpisode);
 });
